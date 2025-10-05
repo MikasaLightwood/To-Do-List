d7603bc7ecfd407225121025971f699c3b1bba0b
Esther Gathatwa <gathatwaesther6@gmail.com>
2025-10-05 22:19:35 +0300

    Add a markdone prototype and create a ToDoList Constructor

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ function Task(description) {
   this.description = description;
   this.isDone = false;
 }
+
+// Mark as done
+Task.prototype.markDone = function() {
+  this.isDone = true;
+};
+
+// --- ToDoList Constructor ---
+function ToDoList() {
+  this.tasks = [];
+}
