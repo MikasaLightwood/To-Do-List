0e5cc6bf373e76dda741f6b96ceae0c8bd625e0d
Esther Gathatwa <gathatwaesther6@gmail.com>
2025-10-05 22:23:23 +0300

    Create the Main App Logic

--- script.js
+++ script.js
@@ -23,3 +23,17 @@ ToDoList.prototype.addTask = function(task) {
 ToDoList.prototype.removeTask = function(description) {
   this.tasks = this.tasks.filter(t => t.description !== description);
 };
+
+// --- Main App Logic ---
+const myList = new ToDoList();
+
+// Pre-filled tasks
+myList.addTask(new Task("Finish JavaScript homework"));
+myList.addTask(new Task("Organize photo folder"));
+myList.addTask(new Task("Call Mom"));
+myList.addTask(new Task("Plan weekend project"));
+myList.addTask(new Task("Drink more water 💧"));
+
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
